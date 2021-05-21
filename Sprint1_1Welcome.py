5b5f3e804f67b4b9571aac64b1f1a6bdc7ee0c7f
patchigalla <61434749+patchigalla@users.noreply.github.com>
2021-05-21 12:30:38 +1200

    changed instace variable names

---
+++ Sprint1_1Welcome.py
@@ -0,0 +1,31 @@
+# welcome.py
+# First screen to inform users about the programme
+# P.Patchigalla Feb 21
+
+# Import all tool kit interface (tkinter) modules
+from tkinter import *
+from tkinter import ttk
+
+
+# Parent class
+class MathQuiz:
+    def __init__(self, parent):
+      
+      self.welcome = Frame(parent)
+      self.welcome.grid(row=0, column=0)
+
+      self.title_label = Label(self.welcome, text="welcome to Maths Quiz",
+                                bg="black", fg="white", width=20, padx=30, pady=10,
+                                font=("Time", '14', "bold italic"))
+      self.title_label.grid(columnspan=2)  # Lable spans over two columns
+
+      self.next_button = ttk.Button(self.welcome, text='Next')  # ttk prefit gives modern Button
+      self.next_button.grid(row=8, column=1)
+
+
+# Main routine
+if __name__ == "__main__":  # checks if condition - name of Parent class is main module
+    root = Tk() 
+    frames = MathQuiz(root) 
+    root.title("Quiz")
+    root.mainloop()  # binds the above commands togeter
